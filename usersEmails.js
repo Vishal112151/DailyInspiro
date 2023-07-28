199e4d096852c16757a55e877d0f948bc7d8b68e
vishalkumarrao <vishalbth99@gmail.com>
2023-07-28 22:58:47 +0530

    ..

--- usersEmails.js
+++ usersEmails.js
@@ -1,21 +1,5 @@
 const emails = [
-  "amanpalsinghrana27@gmail.com",
-  "roushank559@gmail.com",
-  "mk4mbgp@gmail.com",
-  "winnersujeet@gmail.com",
-  "surajsinghj654@gmail.com",
-  "abishekjha9709nit@gmail.com",
-  "neelbeck4@gmail.com",
-  "vishalbth99@gmail.com",
-  "rishavk685@gmail.com",
-  "keshavkumarshivanshu3@gmail.com",
-  "pawanchaudhary026@gmail.com",
-  "rajankmr117@gmail.com",
-  "ankitbaranwal1432@gmail.com",
-  "mralok369@gmail.com",
-  "mk4mbgp@gmail.com",
-  "pavankumar16gupta@gmail.com",
-  "vishalbth99@gmail.com"    
+  "vishalmanit102@gmail.com"  
 ];
 module.exports = emails;
  
